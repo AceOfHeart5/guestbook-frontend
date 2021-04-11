400571ca64897659250b4471c044a0f9ba279731
AceOfHeart5 <evanconwaymusic@gmail.com>
2021-04-11 02:04:49 -0400

    add rough draft, adding messages broken

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const backendurl = 'https://guestbook-backend-a2tlv.ondigitalocean.app/backend';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [messages, setMessages] = useState(null);
+	const [newMessage, setNewMessage] = useState('');
+
+	const getMessages = () => {
+		fetch(backendurl + '/getmessages')
+			.then(res => res.json())
+			.then(res => setMessages(res))
+			.catch(err => console.log(err));
+	}
+
+	useEffect(() => {
+		getMessages();
+	}, []);
+
+	const renderMessages = () => {
+		if (messages === null) {
+			return (
+				<div>Fetching Messages...</div>
+			);
+		}
+		const result = [];
+		messages.forEach(msg => {
+			result.push(<li key={msg.id}>{msg.entry}</li>);
+		});
+		return (<ul>{result}</ul>);
+	}
+
+	const addMessage = (e) => {
+		e.preventDefault();
+		if (newMessage.length <= 0) return;
+		const entry = {
+			entry: newMessage
+		}
+		const options = {
+			method: 'POST',
+			body: JSON.stringify(entry)
+		}
+		console.log(options);
+		fetch(backendurl + '/addmessage', options)
+			.then(res => res.json())
+			.then(res => console.log(res))
+			.then(() => getMessages())
+			.catch(err => console.log(err));
+	}
+
+	return (
+		<div className="App">
+			{renderMessages()}
+			<form onSubmit={addMessage} action="">
+				<input 
+					type="text" 
+					placeholder='Type a message...' 
+					value={newMessage}
+					onChange={e => setNewMessage(e.target.value)}
+				/>
+				<button>Add Message</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
